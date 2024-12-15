8b0664d7927b95faa8505247c7c76bdf46db8805
I Dewa Putu Ardi Nusawan <ardi.nusawan13@gmail.com>
2024-12-15 23:34:22 +0700

    update

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -5,18 +5,18 @@ vim.keymap.set("n", "<C-l>", "<Cmd>NvimTmuxNavigateRight<CR>", {})
 vim.keymap.set("n", "<C-\\>", "<Cmd>NvimTmuxNavigateLastActive<CR>", {})
 vim.keymap.set("n", "<C-Space>", "<Cmd>NvimTmuxNavigateNext<CR>", {})
 
-local actions = require("telescope.actions")
-require("telescope").setup({
-  defaults = {
-    mappings = {
-      i = {
-        ["<C-j>"] = actions.move_selection_next, -- Move down
-        ["<C-k>"] = actions.move_selection_previous, -- Move up
-      },
-      n = {
-        ["<C-j>"] = actions.move_selection_next, -- Move down
-        ["<C-k>"] = actions.move_selection_previous, -- Move up
-      },
-    },
-  },
-})
+-- local actions = require("telescope.actions")
+-- require("telescope").setup({
+--   defaults = {
+--     mappings = {
+--       i = {
+--         ["<C-j>"] = actions.move_selection_next, -- Move down
+--         ["<C-k>"] = actions.move_selection_previous, -- Move up
+--       },
+--       n = {
+--         ["<C-j>"] = actions.move_selection_next, -- Move down
+--         ["<C-k>"] = actions.move_selection_previous, -- Move up
+--       },
+--     },
+--   },
+-- })
